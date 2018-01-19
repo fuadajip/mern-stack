e52d0fbdeeee86523d1af2c543325f212766537b
fuadajip <fuadajipratomo@gmail.com>
2018-01-20 01:44:25 +0700

    AddItem component

---
+++ frontend/src/components/AddItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AddItem extends React.Component {
+    render () {
+        return(
+            <div> 
+                <h2>Add Item</h2>
+            </div>
+        )
+    }
+}
+
+export default AddItem;
