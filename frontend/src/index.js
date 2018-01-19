be5e3a98cd86d843c5c96b3c6024aaf8a3a480fb
fuadajip <fuadajipratomo@gmail.com>
2018-01-20 01:35:36 +0700

    Add simple route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import App from './App';
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path="/" component={App} />
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
