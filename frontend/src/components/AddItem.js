710a6da4025c3d45d74adcd2e5ed6bda09a66d90
fuadajip <fuadajipratomo@gmail.com>
2018-01-20 02:03:53 +0700

    Add handle event AddItem

--- frontend/src/components/AddItem.js
+++ frontend/src/components/AddItem.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
 class AddItem extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { value : ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    handleSubmit(e) {
+        alert(this.state.value);
+        e.preventDefault();
+    }
+
     render () {
         return(
-            <div> 
-                <h2>Add Item</h2>
+            <div className="container"> 
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                    Add Item: 
+                    <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control"/>
+                    </label>
+                    <br/>
+                    <input type="submit" value="Submit" className="btn btn-primary"/>
+                </form>
             </div>
         )
     }
